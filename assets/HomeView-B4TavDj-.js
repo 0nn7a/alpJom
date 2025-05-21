7b33b2a0063e46942c14b5ccb5445411fa1960b7
0nn7a <0nn7a@users.noreply.github.com>
2025-05-21 10:58:38 +0000

    deploy: 88bb03f1b728a3c1970880eab3ad9c61ae3c2f85

---
+++ assets/HomeView-B4TavDj-.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-4JKY_Jyn.js";const o="/alpJom/assets/arcade-CSZ5YGtr.png",s={},a={src:o,alt:"arcade"};function r(_,n){return t(),c("img",a)}const d=e(s,[["render",r],["__scopeId","data-v-efc9f78f"]]);export{d as default};
